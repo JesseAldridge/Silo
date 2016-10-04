fbb461bd02052a05b7465c125c2b3c1846366699
Jesse Aldridge <JesseAldridge@gmail.com>
2016-10-04 12:47:27 -0700

    1 MB -> 10 MB

--- logrot.py
+++ logrot.py
@@ -7,8 +7,8 @@ def write_rotate(line):
   with open(out_path, 'a') as f:
    f.write('{} {}\n'.format(datetime.utcnow(), line))
 
-  # limit files to 1 MB
-  if os.path.getsize(out_path) > 1024 ** 2:
+  # limit files to 10 MB
+  if os.path.getsize(out_path) > 10 * 1024 ** 2:
     shutil.move(out_path, out_path + '.old')
 
 def read_forever():
