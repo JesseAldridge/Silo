706647e6936e77ddae8e4a6569158ad844719dfc
Jesse Aldridge <JesseAldridge@gmail.com>
2017-03-28 19:23:33 -0700

    format_exc

--- logrot.py
+++ logrot.py
@@ -32,4 +32,4 @@ if __name__ == '__main__':
   except:
     with open('logrot.err', 'a') as f:
       f.write('logrot exception\n')
-      traceback.print_exc()
+      f.write(traceback.format_exc())
